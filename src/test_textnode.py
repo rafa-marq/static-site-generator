2dea0164f85394999bdea99a180c371e62374218
rafa-marq <auadarafael@gmail.com>
2025-12-27 11:55:16 -0300

    Edited TextNode tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -24,11 +24,11 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
         self.assertEqual(node, node2)
 
-    # def test_repr(self):
-        # node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
-        # self.assertEqual(
-        #    "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
-        #)
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 class TextType(Enum):
-    TEXT = "plain text"
-    BOLD = "bold text"
-    ITALIC = "italic text"
-    CODE = "code text"
-    LINK = "links"
-    IMAGE = "images"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -24,4 +24,4 @@ class TextNode:
         )
     
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
